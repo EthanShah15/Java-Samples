86e337c3667991921f6ee2599a444b203dc50f46
EthanShah15 <83880320+EthanShah15@users.noreply.github.com>
2023-08-13 22:50:03 -0700

    added 8 queens checker

--- src/EightQueens.java
+++ src/EightQueens.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 public class EightQueens {
     public static void main(String[] args) throws IOException {
         String[] queenPosition1 = readInputString("Enter queen 1's position").split(",");
@@ -11,24 +12,114 @@ public class EightQueens {
         String[] queenPosition6 = readInputString("Enter queen 6's position").split(",");
         String[] queenPosition7 = readInputString("Enter queen 7's position").split(",");
         String[] queenPosition8 = readInputString("Enter queen 8's position").split(",");
-        String[][] grid = new String[8][8];
-        grid[0] = queenPosition1;
-        grid[1] = queenPosition2;
-        grid[2] = queenPosition3;
-        grid[3] = queenPosition4;
-        grid[4] = queenPosition5;
-        grid[5] = queenPosition6;
-        grid[6] = queenPosition7;
-        grid[7] = queenPosition8;
-
+        String[] queenPositions = new String[8];
+        queenPositions[0] = queenPosition1[0] + "," + queenPosition1[1];
+        queenPositions[1] = queenPosition2[0] + "," + queenPosition2[1];
+        queenPositions[2] = queenPosition3[0] + "," + queenPosition3[1];
+        queenPositions[3] = queenPosition4[0] + "," + queenPosition4[1];
+        queenPositions[4] = queenPosition5[0] + "," + queenPosition5[1];
+        queenPositions[5] = queenPosition6[0] + "," + queenPosition6[1];
+        queenPositions[6] = queenPosition7[0] + "," + queenPosition7[1];
+        queenPositions[7] = queenPosition8[0] + "," + queenPosition8[1];
+        boolean noXConflict = checkXValues(queenPositions);
+        boolean noYConflict = checkYValues(queenPositions);
+        boolean noDiagonalConflict = checkDiagonalValues(queenPositions);
+        if (noXConflict && noYConflict && noDiagonalConflict) {
+            System.out.println("These values work");
+        } else {
+            System.out.println("These values don't work");
+        }
     }
+
+    public static int getX(String queenPosition) {
+        return Integer.parseInt(queenPosition.split(",")[0]);
+    }
+
+    public static int getY(String queenPosition) {
+        return Integer.parseInt(queenPosition.split(",")[1]);
+    }
+
+    public static boolean checkXValues(String[] queenPositions) {
+        boolean noConflict = true;
+        for (int i = 0; i < queenPositions.length; i++) {
+            for (int j = i + 1; j < queenPositions.length; j++) {
+                int currentXValue = getX(queenPositions[i]);
+                int checkingValue = getX(queenPositions[j]);
+                if (currentXValue >= 0 && checkingValue >= 0 && currentXValue < 8 && checkingValue < 8) {
+                    if (currentXValue == checkingValue) {
+                        System.out.println(i + " and " + j + " are conflicting on the x axis");
+                        return false;
+                    }
+                }
+            }
+        }
+        return noConflict;
+    }
+
+    public static boolean checkYValues(String[] queenPositions) {
+        boolean noConflict = true;
+        for (int i = 0; i < queenPositions.length; i++) {
+            for (int j = i + 1; j < queenPositions.length; j++) {
+                int currentYValue = getY(queenPositions[i]);
+                int checkingValue = getY(queenPositions[j]);
+                if (currentYValue >= 0 && checkingValue >= 0 && currentYValue < 8 && checkingValue < 8) {
+                    if (currentYValue == checkingValue) {
+                        System.out.println(i + " and " + j + " are conflicting on the y axis");
+                        return false;
+                    }
+                }
+            }
+        }
+        return noConflict;
+    }
+
+    public static boolean checkDiagonalValues(String[] queenPositions) {
+        boolean noConflict = true;
+        for (int i = 0; i < queenPositions.length; i++) {
+            for (int j = i + 1; j < queenPositions.length; j++) {
+                int currentXValue = getX(queenPositions[i]);
+                int currentYValue = getY(queenPositions[i]);
+                int checkingXValue = getX(queenPositions[j]);
+                int checkingYValue = getY(queenPositions[j]);
+                if (currentXValue >= 0 && currentYValue >= 0 && currentXValue < 8 && currentYValue < 8 && checkingXValue >= 0 && checkingYValue >= 0 && checkingXValue < 8 && checkingYValue < 8) {
+                    for (int increment = 1; increment < queenPositions.length; increment++) {
+                        if ((currentXValue + 1) == checkingXValue && (currentYValue + 1) == checkingYValue) {
+                            System.out.println(currentXValue + " and " + checkingXValue + " are conflicting with " + currentYValue + " and " + checkingYValue);
+                            return false;
+                        }
+                    }
+                    for (int increment = 1; increment < queenPositions.length; increment++) {
+                        if ((currentXValue - increment) == checkingXValue && (currentYValue - increment) == checkingYValue) {
+                            System.out.println(currentXValue + " and " + checkingXValue + " are conflicting with " + currentYValue + " and " + checkingYValue);
+                            return false;
+                        }
+                    }
+                    for (int increment = 1; increment < queenPositions.length; increment++) {
+                        if ((currentXValue + increment) == checkingXValue && (currentYValue - increment) == checkingYValue) {
+                            System.out.println(currentXValue + " and " + checkingXValue + " are conflicting with " + currentYValue + " and " + checkingYValue);
+                            return false;
+                        }
+                    }
+                    for (int increment = 1; increment < queenPositions.length; increment++) {
+                        if ((currentXValue - increment) == checkingXValue && (currentYValue + increment) == checkingYValue) {
+                            System.out.println(currentXValue + " and " + checkingXValue + " are conflicting with " + currentYValue + " and " + checkingYValue);
+                            return false;
+
+                        }
+                    }
+                }
+            }
+        }
+        return noConflict;
+    }
+
+
     public static String readInputString(String prompt) throws IOException {
         System.out.println(prompt);
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
 
         // Reading data using readLine
-        String input = reader.readLine();
-        return input;
+        return reader.readLine();
     }
 }
